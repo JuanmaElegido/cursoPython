673e141c1e355d670a90f9101e6b60499b03bfc3
JUANMAELEGIDO <juanmaelegido@gmail.com>
2021-09-18 00:39:38 +0200

    Ejercicio 1 py

--- type.py
+++ type.py
@@ -1,3 +1,10 @@
 type(3)
 var = 5 + 3
 print("var")
+
+print(var)
+
+nombre=input("Dime tu nombre:")
+edad=int (input("Dime tu edad:"))
+edadmas = edad +10
+print("Hola "+nombre+", tu edad dentro de 10 años será: "+edadmas)
